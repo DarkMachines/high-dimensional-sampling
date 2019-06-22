57a6eeb4f9275069fe36c114948a73bb2f111203
Bob Stienen <b.stienen@science.ru.nl>
2019-06-22 15:42:59 +0200

    Add documentation

--- high_dimensional_sampling/functions.py
+++ high_dimensional_sampling/functions.py
@@ -1,11 +1,24 @@
-from abc import ABCMeta, abstractmethod
+from abc import ABC, abstractmethod
 import numpy as np
 import pandas as pd
 
 from .utils import get_time
 
 
-class TestFunction(metaclass=ABCMeta):
+class TestFunction(ABC):
+    """
+    Abstract base class for test functions
+
+    This class forms the basis for all testfunctions implemented in this 
+    module. It automatically tracks the number of evaluations of the function
+    and does dimensionality checks on the input data.
+
+    As this is an abstract class, instances of this class cannot be created or
+    used.
+
+    Raises:
+        Exception: Testfunction should define ranges.
+    """
     def __init__(self):
         if not hasattr(self, 'is_differentiable'):
             self.is_differentiable = False
@@ -16,6 +29,25 @@ class TestFunction(metaclass=ABCMeta):
         self.counter_derivatives = []
 
     def __call__(self, x, derivative=False):
+        """
+        Request an evaluation of the (derivative of the) testfunction.
+
+        After having performed dimensionality and ranges checks on the 
+        provided parameters x, the testfunction in the derived class is queried
+        for its value or its derivative (depending on the request by the user).
+        In this proces the query time is stored for later logging.
+
+        Args:
+            x: numpy.ndarray of shape (nDatapoints, nVariables) containing the
+                datapoints to query to the testfunction
+            derivative: If this boolean is False (default), the testfunction 
+                will be queried for its value. If it is True, the derivative
+                of the function is queried (if existing).
+
+        Returns:
+            A numpy.ndarray of shape (nDatapoints, ?) containing the requested
+            values.
+        """
         # Check if testfunction is fully configured
         self.check_configuration()
         # Convert input information to numpy array
@@ -23,8 +55,6 @@ class TestFunction(metaclass=ABCMeta):
         # Check if dimensionality and ranges of the input are correct
         self.check_dimensionality(x.shape)
         self.check_ranges(x)
-        # Check if function differentiability is compatible with request
-        self.check_differentiability(derivative)
         # Start time for function call
         t_start = get_time()
         # Return requested result(s)
@@ -40,10 +70,26 @@ class TestFunction(metaclass=ABCMeta):
         return value
 
     def reset(self):
-        self.evaluations = [0, 0]
-        self.time = [[], []]
+        """
+        Resets the internal counters of queries
+        """
+        self.counter_time = []
+        self.counter_derivatives = []
 
     def check_configuration(self):
+        """
+        Checks if the testfunction is correctly configured
+
+        As this class is an abstract base class, we want to make sure that the
+        derived class is configured correctly. This method performs sanity
+        checks to make us sure of this. It checks if the function has defined
+        whether or not it has a known derivative and if the ranges to sample
+        are specified. If either of these checks fails, an Exception is raised.
+
+        Raises:
+            Exception: The testfunction has unknown differentiability.
+            Exception: Testfunction has unknown ranges.
+        """
         # Check if is_differentiable is known
         if not hasattr(self, "is_differentiable"):
             raise Exception("The testfunction has unknown differentiability.")
@@ -56,24 +102,53 @@ class TestFunction(metaclass=ABCMeta):
             self.ranges = np.array(self.ranges)
 
     def check_dimensionality(self, shape):
+        """
+        Checks if the dimensionality of the input data is conform expected by 
+        the defined testfunction. If not, an Exception is raised.
+
+        Args:
+            shape: Tuple defining the shape of the input data.
+        
+        Raises:
+            Exception: Provided data has dimensionality ?, but ? was expected.
+        """
         dim = self.ranges.shape[0]
         dim_data = shape[1]
         if dim_data != dim:
             raise Exception("Provided data has dimensionality {}, but {} was expected".format(dim_data, dim))
     
-    def check_ranges(self, x):
+    def check_ranges(self, X):
+        """
+        Checks if the input data lies within the ranges expected by the
+        defined testfunction. If not, an Exception is raised.
+
+        Args:
+            X: numpy.ndarray of shape (nDatapoints, nVariables) containing the
+                data to query the testfunction for.
+        
+        Raises:
+            Exception: Data does not fall withing the expected ranges: ?.
+        """
         # Transform data
-        d = x - self.ranges[:,0]
+        d = X - self.ranges[:,0]
         d = d / (self.ranges[:,1] - self.ranges[:,0])
         # Check if any entry smaller than 0 exists
         if np.any(d < 0.0) or np.any(d > 1.0):
-            raise Exception("Data does not fall within expected ranges: {}".format(self.ranges))
-    
-    def check_differentiability(self, derivative):
-        if derivative and not self.is_differentiable:
-            raise Exception("Derivative could not be calculated: function is not differentiable.")
+            raise Exception("Data does not fall within expected ranges: {}.".format(self.ranges))
 
     def to_numpy_array(self, x):
+        """
+        Converts data to an numpy array
+
+        Converts variables of type list and pandas.DataFrame to a numpy.ndarray
+        and returns these. Variables of type numpy.ndarray are left untouched.
+
+        Args:
+            x: Data of type numpy.ndarray, list or pandas.DataFrame.
+        
+        Returns:
+            Data converted to numpy.ndarray.
+        """
         if isinstance(x, np.ndarray):
             return x
         if isinstance(x, list):
@@ -83,18 +158,63 @@ class TestFunction(metaclass=ABCMeta):
 
     @abstractmethod
     def evaluate(self, x):
+        """
+        Queries the testfunction for a function evaluation.
+
+        This method should be implemented by any testfunction derived from
+        this abstract base class.
+
+        Args:
+            x: Data as a numpy.ndarray of shape (nDatapoints, nVariables) for
+                which the testfunction should be evaluated.
+        
+        Returns:
+            Values returned by the function evaluation as numpy.ndarray of
+            shape (nDatapoints, ?).
+        """
         pass
     
     @abstractmethod
     def derivative(self, x):
+        """
+        Queries the testfunction for its derivative at the provided point(s).
+        
+        This method should be implemented by any testfunction derived from
+        this abstract base class.
+
+        Args:
+            x: Data as a numpy.ndarray of shape (nDatapoints, nVariables) for
+                which the gradient of the testfunction should be returned.
+        
+        Returns:
+            Gradient of the testfunction at the provided location as
+            numpy.ndarray of shape (nDatapoints, ?).
+        
+        Raises:
+            NoDerivativeError: No derivative is known for this testfunction.
+        """
         pass
 
 
 class NoDerivativeError(Exception):
+    """
+    Exception indicating no derivative is known to queried testfunction
+    """
     pass
 
 
 class Sphere(TestFunction):
+    """
+    Testfunction that returns the squared euclidean distance on evaluation. 
+
+    Testfunction following the formula:
+
+        y = sum_{i=1}^{N} x_i^2
+    
+    The derivative of this function is implemented as
+
+        y' = 2*x
+    """
     def __init__(self, dimensionality=3):
         self.is_differentiable = True
         self.ranges = []
@@ -109,6 +229,12 @@ class Sphere(TestFunction):
         return 2*x
 
 class Ackley(TestFunction):
+    """
+    Ackley function as defined by
+    https://en.wikipedia.org/wiki/Ackley_function.
+
+    No derivative has been implemented.
+    """
     def __init__(self):
         self.is_differentiable = False
         self.ranges = [[-5, 5], [-5, 5]]
@@ -126,13 +252,21 @@ class Ackley(TestFunction):
 
 
 class Easom(TestFunction):
+    """
+    Easom function as defined by
+    https://en.wikipedia.org/wiki/Test_functions_for_optimization
+
+    No derivative has been implemented.
+    """
     def __init__(self):
         self.is_differentiable = False
         self.ranges = [[-100, 100], [-100, 100]]
         super(Easom, self).__init__()
     
     def evaluate(self, x):
-        return -1*np.cos(x[:,0])*np.cos(x[:,1])*np.exp(-1*(np.power(x[:,0]-np.pi, 2) + np.power(x[:,1]-np.pi, 2)))
+        return (-1 * np.cos(x[:,0]) * np.cos(x[:,1])
+                * np.exp(-1*(np.power(x[:,0]-np.pi, 2)
+                + np.power(x[:,1]-np.pi, 2))))
     
     def derivative(self, x):
         raise NoDerivativeError()
