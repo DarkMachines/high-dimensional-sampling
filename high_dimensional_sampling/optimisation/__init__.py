773b5377d958237c6a2f66873e81d4bae4c9f491
Bob Stienen <b.stienen@science.ru.nl>
2020-01-08 22:32:56 +0100

    flake8

--- high_dimensional_sampling/optimisation/__init__.py
+++ high_dimensional_sampling/optimisation/__init__.py
@@ -1,5 +1,5 @@
 from .randomoptimisation import RandomOptimisation  # noqa: F401
 
-""" The `pyscannerbit` implementation is excluded from unit tests, as we 
+""" The `pyscannerbit` implementation is excluded from unit tests, as we
 -- unfortunately -- did not get pyscannerbit to work within Travis """
 from .pyscannerbit import PyScannerBit  # noqa: F401
