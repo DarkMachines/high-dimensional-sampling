75e9b533b4eac8214bf435b850b46c12d0778e20
Bob Stienen <b.stienen@science.ru.nl>
2020-01-14 18:50:35 +0100

    Correct flake8 error

--- high_dimensional_sampling/optimisation/__init__.py
+++ high_dimensional_sampling/optimisation/__init__.py
@@ -1,6 +1,6 @@
 from .randomoptimisation import RandomOptimisation  # noqa: F401
 from .ampgo import Ampgo  # noqa: F401
 
-""" The `pyscannerbit` implementation is excluded from unit tests, as we 
+""" The `pyscannerbit` implementation is excluded from unit tests, as we
 -- unfortunately -- did not get pyscannerbit to work within Travis """
-#from .pyscannerbit import PyScannerBit  # noqa: F401
+# from .pyscannerbit import PyScannerBit  # noqa: F401
