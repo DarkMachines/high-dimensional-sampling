608d2303855a3fe31ba2eee329c80c16e7997763
Bob Stienen <b.stienen@science.ru.nl>
2020-01-14 18:47:19 +0100

    Correct unit test errors

--- tests/test_implementations.py
+++ tests/test_implementations.py
@@ -1,9 +1,7 @@
-import inspect
 import shutil
 from high_dimensional_sampling.posterior import RejectionSampling
 from high_dimensional_sampling.optimisation import RandomOptimisation, Ampgo
 from high_dimensional_sampling import functions as func
-from high_dimensional_sampling import procedures as proc
 from high_dimensional_sampling import experiments as exp
 
 
@@ -17,7 +15,7 @@ def test_posterior_implementations(tmp_path):
 
 
 def test_optimisation_implementations(tmp_path):
-    classes = [RandomOptimisation]
+    classes = [RandomOptimisation, Ampgo]
     for procedure_class in classes:
         procedure = procedure_class()
         experiment = exp.OptimisationExperiment(procedure, str(tmp_path))
