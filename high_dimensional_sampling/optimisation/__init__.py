a978e26d8310f3b91171494a6d20ef1f8012ceab
Bob Stienen <b.stienen@science.ru.nl>
2020-01-30 16:04:59 +0100

    Add particle filter optimisation algorithm

--- high_dimensional_sampling/optimisation/__init__.py
+++ high_dimensional_sampling/optimisation/__init__.py
@@ -1,4 +1,5 @@
 from .randomoptimisation import RandomOptimisation  # noqa: F401
+from .particlefilter import ParticleFilter  # noqa: F401
 
 """ The `pyscannerbit` implementation is excluded from unit tests, as we
 -- unfortunately -- did not get pyscannerbit to work within Travis """
