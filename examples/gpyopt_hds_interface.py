85e8879c70dd197ee355a740955544cb3e7f7bfb
ZacSpec <54191768+ZacSpec@users.noreply.github.com>
2020-02-04 09:38:12 +1030

    Create gpyopt_hds_interface.py

---
+++ examples/gpyopt_hds_interface.py
@@ -0,0 +1,21 @@
+# How to run:
+# 1) sudo apt-get install python-pip
+# 2) pip install gpyopt
+
+import high_dimensional_sampling as hds
+from high_dimensional_sampling.optimisation import PyScannerBit
+
+
+procedure = HdsGpyoptInterface(initial_design_numdata=5,
+                               aquisition_type='EI',
+                               exact_feval=True,
+                               de_duplication=True,
+                               num_cores=2,
+                               max_iter=1000,
+                               max_time=60,
+                               eps=1e-6)
+experiment = hds.OptimisationExperiment(procedure, './hds')
+feeder = hds.functions.FunctionFeeder()
+feeder.load_function('Rastrigin', {'dimensionality': 5})
+for function in feeder:
+    experiment.run(function, finish_line=1000)
