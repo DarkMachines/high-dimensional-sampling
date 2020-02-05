a70f1926c08ca9bcf842cefacf41e0042db53937
ZacSpec <54191768+ZacSpec@users.noreply.github.com>
2020-02-06 09:24:52 +1030

    Naming convention mark 2

--- high_dimensional_sampling/optimisation/__init__.py
+++ high_dimensional_sampling/optimisation/__init__.py
@@ -1,6 +1,6 @@
 from .randomoptimisation import RandomOptimisation  # noqa: F401
 from .cmaesoptimisation import CMAOptimisation  # noqa: F401
-from .randomoptimisation import Gpyopt  # noqa: F401
+from .randomoptimisation import GPyOpt  # noqa: F401
 
 """ The `pyscannerbit` implementation is excluded from unit tests, as we
 -- unfortunately -- did not get pyscannerbit to work within Travis """
