082e30d4824c4dea7ae7ef5f7b8330e38faaf7f8
Bob Stienen <bobstienen@gmail.com>
2020-02-13 16:53:14 +0100

    Correct mistakes in test script

--- tests/test_implementations.py
+++ tests/test_implementations.py
@@ -1,12 +1,15 @@
+import inspect
 import shutil
-from high_dimensional_sampling.posterior import RejectionSampling
-from high_dimensional_sampling.optimisation import RandomOptimisation, Ampgo
+from high_dimensional_sampling import posterior
+from high_dimensional_sampling import optimisation
 from high_dimensional_sampling import functions as func
 from high_dimensional_sampling import experiments as exp
+from high_dimensional_sampling import procedures as proc
 
 
 def test_posterior_implementations(tmp_path):
-    classes = [RejectionSampling]
+    all_classes = inspect.getmembers(posterior, inspect.isclass)
+    classes = [c[1] for c in all_classes if isinstance(c[1](), proc.Procedure)]
     for procedure_class in classes:
         procedure = procedure_class()
         experiment = exp.PosteriorSamplingExperiment(procedure, str(tmp_path))
