825e38eac6ed050338a5dc4f9af259cd5d27c775
Bob Stienen <b.stienen@science.ru.nl>
2020-02-19 12:00:31 +0100

    Add test for correct formatting hiddenfunctions

--- tests/test_hiddenfunctions.py
+++ tests/test_hiddenfunctions.py
@@ -1,6 +1,23 @@
 import os
 import numpy as np
-import high_dimensional_sampling.functions as func
+import shutil
+from high_dimensional_sampling import functions as func
+from high_dimensional_sampling import experiments as exp
+from high_dimensional_sampling import optimisation as optim
+
+
+class TmpExperimentCorrect(exp.Experiment):
+    def make_metrics(self):
+        return {}
+
+    def _event_start_experiment(self):
+        pass
+
+    def _event_end_experiment(self):
+        pass
+
+    def _event_new_samples(self, x, y):
+        pass
 
 
 def test_hiddenfunctions_general():
@@ -27,3 +44,11 @@ def test_hiddenfunctions_all():
     function = func.HiddenFunction4()
     x = np.random.rand(1, 16)
     assert function(x).shape[0] == len(x)
+
+
+def test_hiddenfunctions_use():
+    procedure = optim.RandomOptimisation()
+    path = "./tmpexperiment"
+    experiment = TmpExperimentCorrect(procedure, path)
+    experiment.run(func.HiddenFunction1(), log_data=False)
+    shutil.rmtree(path)
