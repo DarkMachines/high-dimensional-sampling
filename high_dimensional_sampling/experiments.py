0735f9a48eb7f004f5fd019c2cc7ee8ccfbe9eaa
Bob Stienen <b.stienen@science.ru.nl>
2020-03-11 16:02:16 +0100

    Solve error with TuRBO output shapes

--- high_dimensional_sampling/experiments.py
+++ high_dimensional_sampling/experiments.py
@@ -96,6 +96,14 @@ class Experiment(ABC):
             t_start = get_time()
             x, y = self.procedure(self.function)
             dt = get_time() - t_start
+            # Reshape output arrays to match expectation
+            if len(x.shape) == 1:
+                if x.shape[0] == self.function.get_dimensionality():
+                    x = x.reshape((1, -1))
+                else:
+                    x = x.reshape((-1, 1))
+            if len(y.shape) == 1:
+                y = y.reshape((len(x), 1))
             self._event_new_samples(x, y)
             # Log procedure call
             n = len(x)
